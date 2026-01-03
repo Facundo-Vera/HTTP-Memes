6a0c7770396cc516ae3f6eb6d79d79ffe7d74d5d
Facundo Vera <facundovera.dev@gmail.com>
2026-01-03 17:40:30 -0300

    Agrego paginacion y el logo de la pagina

--- status-codes-meme/src/App.jsx
+++ status-codes-meme/src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Pagination from "./components/Pagination";
+import logo from "./assets/image/logo.webp"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="min-h-screen w-full bg-black relative flex flex-col">
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background:
+            "radial-gradient(circle at 20% 20%, rgba(255,255,255,0.08) 0%, transparent 40%), radial-gradient(circle at 80% 30%, rgba(255,255,255,0.05) 0%, transparent 40%), linear-gradient(120deg, #0f0e17 0%, #1a1b26 100%)",
+        }}
+      />
 
-export default App
+      <main className="flex-grow z-10">
+        <img src={logo} alt="logo de la pagina " />
+      </main>
+          
+      <div className="z-10 flex justify-center pb-6">
+        <Pagination />
+      </div>
+    </div>
+  );
+};
+
+export default App;
